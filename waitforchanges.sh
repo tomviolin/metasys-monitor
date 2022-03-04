a97a9a2bae08f1e0ac2e0451a18f3ae12d835219
Tom Hansen <tomh@uwm.edu>
2022-03-04 01:20:41 -0600

    robustifying the loading popup

--- waitforchanges.sh
+++ waitforchanges.sh
@@ -2,10 +2,9 @@ while true; do
 	changes=`git status --porcelain|wc -l`
 	if [ "$changes" -gt "0" ]; then
 		git add .
-		git commit -m "csv file export"
+		git commit -m "robustifying the loading popup"
 		git push
 		sleep 5
-		php check_pipeline.php
 	fi
 	sleep 0.5
 done
