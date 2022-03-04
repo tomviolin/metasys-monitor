234b1b4b9f0b9586b539af8993ef40fc968b3b97
Tom Hansen <tomh@uwm.edu>
2022-03-04 03:08:26 -0600

    robustifying the loading popup

--- index.php
+++ index.php
@@ -531,7 +531,8 @@ if (!isset($_GET["query"]) || $_GET["query"] == "selected") {
 					}
 					console.log('each datx...');
 
-					$.each(datx, function(data_index, data) {
+					$.each(datx, function(data) {
+						data_index = data.data_index;
 						data_id = 'dval' + data_index;
 						console.log("in each: data_id=", data_id, "data=", data);
 						if (data.value != ' ') {
