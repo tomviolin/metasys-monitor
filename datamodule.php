baa6caaa0e1f17e3ee12f202755124cf33a5da66
Tom Hansen <tomh@uwm.edu>
2023-10-30 15:11:31 -0500

    fixed datamodule; system works now

--- datamodule.php
+++ datamodule.php
@@ -91,7 +91,7 @@ flush();
 	}
 
 
-	$result = mysql_query("SELECT d.heading,d.dis_functional_name AS dis_functional_name, d.description as dis_description, d.soft_min_value, d.soft_max_value, d.hard_min_value, d.hard_max_value, d.priority, d.alarm_name, d.alarm_type, d.allpoints_recid, a.*, i.ip_address, object_types.object_id AS object_type_id FROM display_points2$DATA_SUFFIX d LEFT JOIN allpoints_postchange_2022 a ON d.functional_name = a.functional_name LEFT JOIN devices i ON a.device_id = i.device_id LEFT JOIN object_types ON a.object_type = object_types.object_name ORDER BY heading, d.recid");
+	$result = mysql_query("SELECT d.heading,d.object_name AS dis_functional_name, d.description as dis_description, d.soft_min_value, d.soft_max_value, d.hard_min_value, d.hard_max_value, d.priority, d.alarm_name, d.alarm_type, d.legacy_allpoints_recid AS allpoints_recid, a.*, i.ip_address, object_types.object_id AS object_type_id FROM display_points2$DATA_SUFFIX d LEFT JOIN allpoints_postchange_2022 a ON d.object_name = a.obj_name LEFT JOIN devices i ON a.device_id = i.device_id LEFT JOIN object_types ON a.obj_type = object_types.object_name ORDER BY heading, d.recid");
 	if (mysql_errno() != 0) die(mysql_error()."\n");
 	$j = array();
 	while ($row = mysql_fetch_array($result)) {
